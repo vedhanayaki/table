66847d35e15a5a39da6867775212cdeeb7898bae
vedhanayaki <vedhanayaki1996@gmail.com>
2017-06-30 11:55:20 +0530

    Add files via upload

---
+++ Multiplicationtable.java
@@ -0,0 +1,14 @@
+package guviset3;
+
+public class Multiplicationtable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 9;
+        for(int i = 1; i <= 20; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", a, i, a * i);
+        }
+	}
+
+}
